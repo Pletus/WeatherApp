142d8a5cd4bd4c9163f3b2f3ae19903b5112dc2a
YagoPazos <yagopb@gmail.com>
2024-05-13 11:10:51 +0200

    added input and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('')
+ 
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+  };
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  
   return (
-    <>
-
-    </>
+    <div className='flex inline-flex gap-4 mt-20'>
+  	  <input 
+        type="text" 
+        onChange={handleInputChange} 
+        value={search} 
+        className='rounded-s-sm text-center drop-shadow-2xl border' 
+        placeholder='Insert your City' 
+      />
+      <button onSubmit={onSubmit} className='rounded-s-sm drop-shadow-2xl'>
+        <img 
+          src="src\assets\icons\search-svgrepo-com.svg" 
+          className='search-btn' 
+          alt="Search City Button" />
+      </button>    
+    </div>
   )
 }
 
